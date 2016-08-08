a3a1717a4764c2ecf6cfd54afbf6279b0435f479
forkiven <mikewu1209@gmail.com>
2016-08-08 23:17:39 +0900

    Fixed landing again

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,29 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Welcome</div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Welcome</div>
 
-                <div class="panel-body">
-                    Your Application's Landing Page.
+                    <div class="panel-body">
+                        Your Application's Landing Page.
 
-                    <h4><a href="/checklist/make">Make a checklist</a></h4>
-                    @if(Auth::user()->checklists)
-                    <ul>
-                        @foreach(Auth::user()->checklists as $checklist)
-                        <li>
-                            <a href="/checklist/{{ hashId($checklist) }}">{{ $checklist->name }}</a>
-                        </li>
-                            @endforeach
-                    </ul>
+                        <h4><a href="/checklist/make">Make a checklist</a></h4>
+                        @if(Auth::check() && Auth::user()->checklists)
+                            <ul>
+                                @foreach(Auth::user()->checklists as $checklist)
+                                    <li>
+                                        <a href="/checklist/{{ hashId($checklist) }}">{{ $checklist->name }}</a>
+                                    </li>
+                                @endforeach
+                            </ul>
                         @endif
 
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
