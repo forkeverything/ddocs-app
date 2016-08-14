1fc4cda406c66f004406a771ef7e3171a94be529
forkiven <mikewu1209@gmail.com>
2016-08-15 05:08:31 +0900

    gulp'd for ga tracker

--- public/js/app/dependencies.js
+++ public/js/app/dependencies.js
@@ -7,6 +7,13 @@ $.ajaxSetup({
 $(document).ready(function () {
     autosize($('.autosize'));
 });
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-82446082-1', 'auto');
+ga('send', 'pageview');
 
 /**
  * Takes string and capitalizes the first letter
@@ -231,6 +238,39 @@ function isNumeric(n) {
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
 })
+Vue.directive('datepicker', {
+    params: ['button-only'],
+    bind: function() {
+        if(this.params.buttonOnly) {
+            $(this.el).datepicker({
+                dateFormat: "dd/mm/yy",
+                minDate: 0,
+                buttonImage: '/images/icons/calendar.png',
+                buttonImageOnly: true,
+                showOn: 'both'
+            });
+        } else {
+            $(this.el).datepicker({
+                dateFormat: "dd/mm/yy",
+                minDate: 0
+            });
+        }
+    }
+});
+Vue.directive('modal', {
+    twoWay: true,
+    update: function () {
+        var self = this;
+        
+        $(this.el).click(function () {
+            self.set(false);
+        });
+
+        $(this.el).children().click(function (e) {
+            e.stopPropagation();
+        });
+    }
+});
 Vue.component('date-range-field', {
     name: 'dateRangeField',
     template: '<div class="date-range-field" @click.stop="">' +
@@ -561,39 +601,6 @@ Vue.component('per-page-picker', {
     ready: function () {
         this.$watch('currentItemsPerPage', function (numItems) {
             this.newItemsPerPage = numItems;
-        });
-    }
-});
-Vue.directive('datepicker', {
-    params: ['button-only'],
-    bind: function() {
-        if(this.params.buttonOnly) {
-            $(this.el).datepicker({
-                dateFormat: "dd/mm/yy",
-                minDate: 0,
-                buttonImage: '/images/icons/calendar.png',
-                buttonImageOnly: true,
-                showOn: 'both'
-            });
-        } else {
-            $(this.el).datepicker({
-                dateFormat: "dd/mm/yy",
-                minDate: 0
-            });
-        }
-    }
-});
-Vue.directive('modal', {
-    twoWay: true,
-    update: function () {
-        var self = this;
-        
-        $(this.el).click(function () {
-            self.set(false);
-        });
-
-        $(this.el).children().click(function (e) {
-            e.stopPropagation();
         });
     }
 });
