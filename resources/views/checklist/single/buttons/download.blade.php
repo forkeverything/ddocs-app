f6743ff0554ae3887478c8842569e67892dbd284
forkiven <mikewu1209@gmail.com>
2016-08-17 08:14:54 +0900

    fixed download link again

--- resources/views/checklist/single/buttons/download.blade.php
+++ resources/views/checklist/single/buttons/download.blade.php
@@ -1,6 +1,6 @@
 <!-- Download -->
-<a :href="'/' + file.uploads[(file.length - 1)].path" :alt="file.name + 'download link'" :download="file.name + '_v' + file.version + '_' + getUploadDate(file.uploads[(file.length - 1)])"
-   v-if="file.uploads[(file.length - 1)]">
+<a :href="'/' + file.uploads[(file.uploads.length - 1)].path" :alt="file.name + 'download link'" :download="file.name + '_v' + file.version + '_' + getUploadDate(file.uploads[(file.uploads.length - 1)])"
+   v-if="file.uploads[(file.uploads.length - 1)]">
     <button type="button" class="btn btn-unstyled button-download"><i class="fa fa-download"></i>
     </button>
 </a>
