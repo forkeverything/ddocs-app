b9a3b8a04c25fbec774c69527eaa309f036264bb
forkiven <mikewu1209@gmail.com>
2016-08-29 18:45:25 +0900

    Added checklist email route and pheanstalk

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,9 @@ use Illuminate\Http\Request;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
+
+
+Route::post('/checklist/make/email', function (Request $request) {
+//    'ChecklistsController@postNewChecklistFromEmailWebhook'
+    Log::info($request->all());
+});
