f36585545fd8afd4557efe5a37480a65aca2fc60
forkiven <mikewu1209@gmail.com>
2016-08-31 16:52:30 +0900

    DEPLOY LIVE AT: 31/08/2016/04:52:11 pm

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
+ #!/bin/sh
 now="$(date +'%d/%m/%Y/%r')"
 gulp --production
 git add .
 git commit -m "DEPLOY LIVE AT: $now"
 git push origin master
-sleep 5
 curl https://forge.laravel.com/servers/95640/sites/218391/deploy/http?token=xjsheQDswocGPSN4YDnczazkcOJNhPGMSkvHpXiB
